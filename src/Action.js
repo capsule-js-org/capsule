f4093f5e53cf20ad5f96e8de16af0350c8a021b8
Aikar <aikar@aikar.co>
2017-02-19 20:31:01 -0500

    start framing out actions and capsules, clean up license

--- src/Action.js
+++ src/Action.js
@@ -1,37 +1,19 @@
 /*
- * Copyright (c) 2017 - present Daniel Ennis (Aikar) - MIT License
- *
- *  Permission is hereby granted, free of charge, to any person obtaining
- *  a copy of this software and associated documentation files (the
- *  "Software"), to deal in the Software without restriction, including
- *  without limitation the rights to use, copy, modify, merge, publish,
- *  distribute, sublicense, and/or sell copies of the Software, and to
- *  permit persons to whom the Software is furnished to do so, subject to
- *  the following conditions:
- *
- *  The above copyright notice and this permission notice shall be
- *  included in all copies or substantial portions of the Software.
- *
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- *  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
- *  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- *  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- *  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * @flow
+ * Copyright (c) 2017 Daniel Ennis (Aikar) - MIT License - http://opensource.org/licenses/MIT
  */
+import Capsule from "./Capsule";
 
-/*@flow*/
 export class Action {
-  get(...args:any[]): Promise<any> {
-    return Promise.resolve();
-  }
-  update(...args:any[]): Promise<any> {
-    return Promise.resolve();
-  }
-  observe(...args:any[]): Promise<any> {
-    return Promise.resolve();
+  constructor(def: ActionDef, verify: Symbol) {
+    if (!Capsule.isValidAction(verify)) {
+      throw new Error("Please use capsule.createAction to create an action.");
+    }
   }
 }
 
+export class ActionDef {
+  update: ?(...values: any) => Promise<any>;
+  retrieve: ?() => Promise<any>;
+}
 export default Action;
--- src/Capsule.js
+++ src/Capsule.js
@@ -1,35 +1,51 @@
 /*
- * Copyright (c) 2017 - present Daniel Ennis (Aikar) - MIT License
- *
- *  Permission is hereby granted, free of charge, to any person obtaining
- *  a copy of this software and associated documentation files (the
- *  "Software"), to deal in the Software without restriction, including
- *  without limitation the rights to use, copy, modify, merge, publish,
- *  distribute, sublicense, and/or sell copies of the Software, and to
- *  permit persons to whom the Software is furnished to do so, subject to
- *  the following conditions:
- *
- *  The above copyright notice and this permission notice shall be
- *  included in all copies or substantial portions of the Software.
- *
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- *  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
- *  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- *  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- *  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * @flow
+ * Copyright (c) 2017 Daniel Ennis (Aikar) - MIT License - http://opensource.org/licenses/MIT
  */
 
-/* @flow */
+import {ActionDef} from "./Action";
+import Action from "./Action";
+
+const pathSplitRegex = /./;
+const actionVerifySymbol = new Symbol();
+
 export class Capsule {
+  _internalState: {[any]: any};
   constructor(id: String) {
     if (!id) {
       throw new Error("You must specify an ID for a Capsule");
     }
   }
+
+  initialize(state: {[any]: any}): void {
+    this._internalState = state;
+  }
+
+  toJson(): string {
+    return JSON.stringify(this._internalState);
+  }
+
+  updateState(path: string, state: any): void {
+    let root = this._internalState;
+    let split = path.split(pathSplitRegex);
+    for (const key of split.slice(0, split.length-2)) {
+      // TODO: Validate exists and error, or set? how to handle this?
+      root = root[key];
+    }
+    root[split[split.length-1]] = state;
+    // TODO: Listeners
+  }
+
+  static createAction(actionDef: ActionDef): Action {
+    // TODO: Validation
+    return new Action(actionDef, actionVerifySymbol);
+  }
+
+  static isValidAction(verify: Symbol): boolean {
+    return verify === actionVerifySymbol;
+  }
 }
 
-export {Action} from "./Action";
+export {Action};
 
 export default Capsule;
